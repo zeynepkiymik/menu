c7b49849b98c8b740f881e7c2f27e0114463d9d7
zeynepkiymik <33024264+zeynepkiymik@users.noreply.github.com>
2021-12-06 14:10:14 +0300

    Create MenuController.php

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    //
+    public function index(){
+        return view('anasayfa');
+    }
+    
+    public function hakkimizda(){
+        return view('hakkimizda');
+    }
+}
+
