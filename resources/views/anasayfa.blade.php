f3b0559d08ba2a4bcf89e5306ab0d5be176aa58d
zeynepkiymik <33024264+zeynepkiymik@users.noreply.github.com>
2021-12-06 14:04:43 +0300

    Create anasayfa.blade.php

---
+++ resources/views/anasayfa.blade.php
@@ -0,0 +1,15 @@
+@extends('master')
+@section('baslik' , 'BLOGDENEME')
+@section('CSS')
+
+<style>
+body {
+    background-color:#dfbe5d
+    
+}
+</style>
+@endsection
+@section('icerik')
+<h1> Anasayfa Sayfası </h1>
+@endsection
+
